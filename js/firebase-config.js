3be3b5d8d32149f60626521273730885c3a20aea
dekelhasid <dekelhasid@gmail.com>
2026-08-03 15:01:02 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,9 @@
-/*
- * Paste the Firebase Web App configuration here after creating the Firebase project.
- * The config is safe to publish; Firebase Rules protect the data.
- */
-window.MAHAKESHER_FIREBASE_CONFIG = null;
+window.MAHAKESHER_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyC1RYjPzTFNeqPr5-mvoGTdm00yK3Uzw80',
+  authDomain: 'mahakesher2nekuda0.firebaseapp.com',
+  databaseURL: 'https://mahakesher2nekuda0-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'mahakesher2nekuda0',
+  storageBucket: 'mahakesher2nekuda0.firebasestorage.app',
+  messagingSenderId: '872697430655',
+  appId: '1:872697430655:web:d3f9b1d73dcb91a58e754a'
+};

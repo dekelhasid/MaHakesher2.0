c0ad6803b18c6524d10c7a699b3e3a1ce86c8375
dekelhasid <dekelhasid@gmail.com>
2026-09-16 15:32:19 +0300

    Delete feedback.js

--- feedback.js
+++
@@ -1,8 +0,0 @@
-import { firebaseConfigured, ensureSignedIn, add } from './firebase.js';
-
-export async function submitFeedback(feedback) {
-  if (!firebaseConfigured) throw new Error('firebase-not-configured');
-  const user = await ensureSignedIn();
-  if (!user) throw new Error('not-signed-in');
-  return add('feedback', feedback);
-}
